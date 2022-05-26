f9f32332a13208ed3cdb039f31f774dd5cd4d0b3
6cypT0r <79215914+6cypT0r@users.noreply.github.com>
2022-05-26 15:33:08 +0500

    Create Mistakes.cs

---
+++ 14/Mistakes.cs
@@ -0,0 +1,13 @@
+public class Student
+{
+    private string name;
+    public string Name { 
+        get { return name; } 
+        set 
+		{ 
+			if(value == null)
+				throw new ArgumentException();
+			name = value; 
+		} 
+    }
+}
